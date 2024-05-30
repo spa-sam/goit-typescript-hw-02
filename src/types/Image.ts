3a64c0b940f4e6767f072c532a50a2848a6a1b55
Pablo <st.spasam@gmail.com>
2024-05-30 19:16:24 +0300

    fix

--- src/types/Image.ts
+++ src/types/Image.ts
@@ -1,69 +1,21 @@
 export interface Image {
   id: string;
-  created_at: string;
-  updated_at: string;
-  promoted_at: string | null;
   width: number;
   height: number;
-  color: string;
-  blur_hash: string;
   description: string | null;
   alt_description: string | null;
   urls: {
-    raw: string;
-    full: string;
     regular: string;
     small: string;
-    thumb: string;
   };
-  links: {
-    self: string;
-    html: string;
-    download: string;
-    download_location: string;
-  };
-  likes: number;
-  liked_by_user: boolean;
-  current_user_collections: any[];
-  sponsorship: any;
-  topic_submissions: any;
   user: {
     id: string;
-    updated_at: string;
     username: string;
     name: string;
-    first_name: string;
-    last_name: string | null;
-    twitter_username: string | null;
-    portfolio_url: string | null;
-    bio: string | null;
-    location: string | null;
-    links: {
-      self: string;
-      html: string;
-      photos: string;
-      likes: string;
-      portfolio: string;
-      following: string;
-      followers: string;
-    };
     profile_image: {
       small: string;
       medium: string;
       large: string;
     };
-    instagram_username: string | null;
-    total_collections: number;
-    total_likes: number;
-    total_photos: number;
-    accepted_tos: boolean;
-    for_hire: boolean;
-    social: {
-      instagram_username: string | null;
-      portfolio_url: string | null;
-      twitter_username: string | null;
-      paypal_email: string | null;
-    };
   };
-  tags: any[];
 }
